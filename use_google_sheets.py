1062f52f8747c5a6e62b37c8a67584547a4f04ed
EmmettBicker <emmettbicker@gmail.com>
2022-10-26 22:17:58 -0400

    Finished first verision, downloads as csv

--- use_google_sheets.py
+++ use_google_sheets.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 from class_assignments import assign
 
 
@@ -34,23 +35,80 @@ def main():
         "Intro to Java" : 2,
         "Intermediate Java" : 3
     }
+
+    id_to_preference = {
+        0 : "Intro to Python",
+        1 : "Intermediate Python",
+        2 : "Intro to Java",
+        3 : "Intermediate Java"
+    }
     student_preferences = [preference_to_id[preference] for preference in student_preferences]
 
     volunteer_class_list, student_to_teacher_ratios, leftover_volunteers, class_popularity = assign(volunteers=volunteer_ids_to_capabilites, students=student_preferences)
 
+    volunteer_to_class = {}
+    for volunteer in volunteer_class_list[0]:
+        volunteer_to_class[id_to_volunteer[volunteer]] = "Intro to Python"
+    for volunteer in volunteer_class_list[1]:
+        volunteer_to_class[id_to_volunteer[volunteer]] = "Intermediate Python"
+    for volunteer in volunteer_class_list[2]:
+        volunteer_to_class[id_to_volunteer[volunteer]] = "Intro to Java"
+    for volunteer in volunteer_class_list[3]:
+        volunteer_to_class[id_to_volunteer[volunteer]] = "Intermediate Java"
+        
     # this code turns the numbers in the class lists to the names (omg this code is so bad)
     volunteer_class_list = [[id_to_volunteer[volunteer_class_list[class_num][j]] for j in range(len(volunteer_class_list[class_num]))] for class_num in range(4)]
     student_names = [data["Student First Name"][i].strip() + " " + data["Student Last Name"][i].strip() for i in range(len(data["Student First Name"]))]
     student_ages = list(data["Student Grade"])
-    print(student_ages)
+    
     students_full = list(zip(student_names,student_preferences,student_ages))
 
     # Sort by age
     students_full = sorted(students_full, key=lambda x : x[2])
-    print(student_to_teacher_ratios)
-
-    # Figure out framework for the assignment
-    name_to_class = dict(zip([name for name in volunteer_names], [ [] for _ in range(len(volunteer_names))]))
-    print(name_to_class)
     
+    
+    # Figure out framework for the assignment
+    volunteer_name_to_class_list = dict(zip(volunteer_names, [ [] for _ in range(len(volunteer_names))]))
+    
+    volunteer_name_and_class = volunteer_names.copy()
+    
+    
+        
+
+    for student in students_full:
+        preference = student[1]
+        student = list(student)
+        student[2] = "Grade " + str(student[2])
+        for volunteer in volunteer_class_list[preference]:
+            if len(volunteer_name_to_class_list[volunteer]) < math.ceil(student_to_teacher_ratios[preference]):
+                del student[1]
+                student = " - ".join(student)
+                volunteer_name_to_class_list[volunteer].append(student)
+                break
+    
+    volunteer_names = sorted(volunteer_names, key=lambda name : preference_to_id[volunteer_to_class[name]])
+    final_class_assignments = pd.DataFrame(columns=volunteer_names)
+    for volunteer in volunteer_names:
+        final_class_assignments[volunteer] = pd.Series(volunteer_name_to_class_list[volunteer])
+    
+    final_class_assignments = final_class_assignments.transpose()
+    final_class_assignments.insert(loc=0,
+          column="Classes",
+          value=[volunteer_to_class[name] for name in volunteer_names])
+    print(final_class_assignments)
+    
+
+    print(final_class_assignments)
+    final_class_assignments.to_csv('assignments.csv')
+    
+    
+    """
+    for entry in volunteer_name_to_class_list:
+        print(entry)
+        for value in volunteer_name_to_class_list[entry]:
+            value = list(value)
+            value[1] = id_to_preference[value[1]]
+            print(value)
+        print()
+    """
 main()
